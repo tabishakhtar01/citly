85209eae47462ed7dddb51e1c3487f559758cced
tabishakhtar01 <tabishakhtar01@gmail.com>
2021-05-22 11:22:07 +0530

    Added shortners controller and index action

---
+++ app/controllers/shortners_controller.rb
@@ -0,0 +1,5 @@
+class ShortnersController < ApplicationController
+    def index
+        render
+    end
+end
---
+++ test/controllers/shortners_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ShortnersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
