efe4faec7e76ccbf10564ad26cbba19625845498
tabishakhtar01 <tabishakhtar01@gmail.com>
2021-05-25 14:40:46 +0530

    resolving find by deployment error

--- app/models/shortner.rb
+++ app/models/shortner.rb
@@ -1,5 +1,5 @@
 class Shortner < ApplicationRecord
     validates :url, presence: true, format: URI::regexp(%w[http https])
-    validates :short_url, presence: true, format: URI::regexp(%w[http https])
     validates :code, presence: true
+    validates :short_url, presence: true, format: URI::regexp(%w[http https])
 end
